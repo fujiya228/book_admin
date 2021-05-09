11ac8577b7669a1d2d535a8b78f401195e5bf962
fujiya228m <fujiya0xE2@gmail.com>
2021-05-09 10:49:08 +0900

    コールバック

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,4 +10,31 @@ class Book < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 15 }
   validates :price, numericality: {greater_than_or_equal_to: 0}
+
+  before_validation do |book|
+    book.name = book.name.gsub(/Cat/) do |matched|
+      "lovely #{matched}"
+    end
+  end
+
+  # メソッド使ってもできるで
+  # before_validation :add_lovely_to_cat
+  # def add_lovely_to_cat
+  #   book.name = book.name.gsub(/Cat/) do |matched|
+  #     "lovely #{matched}"
+  #   end
+  # end
+  
+  after_destroy do |book|
+    Rails.logger.info "Book is deleted: #{book.attributes.inspect}"
+  end
+
+  def high_price?
+    price >= 5000
+  end
+
+  after_destroy :if => :high_price? do |book|
+    Rails.logger.warn "Book with high price is deleted: #{book.attributes.inspect}"
+    Rails.logger.warn "Please check!"
+  end
 end
