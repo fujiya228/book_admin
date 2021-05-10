b2276b69ea720df576c22d5e82143884b7143118
fujiya228m <fujiya0xE2@gmail.com>
2021-05-11 07:35:51 +0900

    Strong Parameters

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,5 +6,18 @@ class ProfileController < ApplicationController
   end
 
   def update
+    user = current.user
+    # params[:user] => {name: "bob", email: "bob@example.com"}
+    # user.update(params[:user]) # これだと変更してはだめなものを変更できる脆弱性
+    user.update(user_params)
+  end
+
+  private
+  # 外部から渡されるパラメータをそのまま信用せず、ホワイトリスト方式で検査
+  def user_params
+    if current_user.admin?
+      params.require(:user).permit(:name, :email, :admin)
+    else
+      params.require(:user).permit(:name, :email)
   end
 end
