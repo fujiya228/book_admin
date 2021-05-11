738ed091fa4288318ddbe908dd073d72230522b4
fujiya228m <fujiya0xE2@gmail.com>
2021-05-12 08:19:02 +0900

    基本の描画

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,13 @@
 class BooksController < ApplicationController
-  before_action: :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
 
   def show
-    @book = Book.find(params[:id])
-    respond_to do |format|
-      format.html
-      format.csv
-      format.json
-    end
+    render :show # 省略もできる（アクション名に一致）
+    # respond_to do |format|
+    #   format.html
+    #   format.csv
+    #   format.json
+    # end
   end
 
   private
